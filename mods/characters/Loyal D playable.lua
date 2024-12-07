be73ff77605bd0532810d53496ac8be3262e7334
VeritonR <67881127+VeritonR@users.noreply.github.com>
2024-12-06 23:02:49 -0300

    Delete mods directory

--- mods/characters/Loyal D playable.lua
+++
@@ -1,8 +0,0 @@
-size = 1
-function onCreatePost()
-    if week == 'week3' then
-        size = 0.7
-        setProperty('boyfriend.flipX', false)
-    end
-    scaleObject('boyfriend', size,size)
-end
